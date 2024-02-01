e8f8305717140e8e307eb0804e0a842a683c38ad
Aditya Ojha <aditya.ojha2820@gmail.com>
2024-01-31 22:42:34 -0500

    code fix

--- recursion/generateSubsets.cpp
+++ recursion/generateSubsets.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void printSub(string str, string curr, int index)
 {	
-	cout<<"Recursive call made "<<endl;
+	cout<<"Recursive call made in this "<<endl;
 	if (index == str.length())
 	{
 		cout << curr << endl;
