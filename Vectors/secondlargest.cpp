0cb1e0029aedb542deb36dcf6e29b8d798e02a71
Aditya Ojha <aditya.ojha2820@gmail.com>
2024-03-04 00:19:26 -0500

    code fix for secondlargest

--- Vectors/secondlargest.cpp
+++ Vectors/secondlargest.cpp
@@ -4,19 +4,25 @@ using namespace std;
 
 int main()
 {
-	vector<int> v = {5,20,12,20,8};
+	vector<int> v = {10, 100, 10132};
 	int largest = 0;
 	int secondlargest = -1;
 
 	for (int i = 0; i < v.size(); i++)
 	{
-		if (v[i] > v[largest])
+		if (v[i] != v[largest])
 		{
-			secondlargest = largest;
-			largest = i;
+			if (v[i] > v[largest])
+			{
+				secondlargest = largest;
+				largest = i;
+			}
+			else if (v[i] > v[secondlargest] && v[i] < v[largest])
+				secondlargest = i;
 		}
-		else if (v[i] > v[secondlargest] && v[i] < v[largest])
-			secondlargest = i;
+		else{
+			v[secondlargest] = -1000000;
+		}
 	}
-	cout << v[secondlargest];
+	cout << v[secondlargest] << endl;
 }
