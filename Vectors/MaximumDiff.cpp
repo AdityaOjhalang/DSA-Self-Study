abe6f3bb712fbfd7e6af03fd868a150ee2e868f4
Aditya Ojha <aditya.ojha2820@gmail.com>
2024-03-22 12:01:48 -0400

    Max difference

---
+++ Vectors/MaximumDiff.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Calculates the maximum difference between any two elements in the given vector.
+ * This function uses an efficient algorithm to find the maximum difference.
+ * 
+ * @param arr The vector of integers to find the maximum difference from.
+ */
+void maxidiff_efficient(vector<int> arr)
+{
+
+	int res = arr[1] - arr[0];
+	int minval = arr[0];
+
+	for (int i = 1; i < arr.size(); i++)
+	{
+		res = max(res, arr[i] - minval);
+		minval = min(minval, arr[i]);
+	}
+	cout << res << endl;
+	cout << minval << endl;
+}
+int main()
+{
+	vector<int> arr = {2, 5, 3, 9, 1}; 
+	maxidiff_efficient(arr);
+
+	return 0;
+}
