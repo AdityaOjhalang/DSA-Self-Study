768b1a034b1541b674e5832be66a3944887f3d3d
Aditya Ojha <aditya.ojha2820@gmail.com>
2024-03-22 22:03:18 -0400

    Trapping rain better added time is best space needs improvement

--- Vectors/trappingRainwater.cpp
+++ Vectors/trappingRainwater.cpp
@@ -20,16 +20,42 @@ int trappingRain_naive(vector<int> arr)
 		}
 		storedWater = storedWater + (min(lmax, rmax) - arr[i]);
 	}
-	cout<< storedWater << endl;
+	cout << storedWater << endl;
 }
-int trappingRain_efficient(vector<int> arr)
+int trappingRain_better(vector<int> arr)
+{
+	int res = 0;
+	int n = arr.size();
+
+	vector<int> lmax(n);
+	vector<int> rmax(n);
+	lmax[0] = arr[0];
+	rmax[n - 1] = arr[n - 1];
+	for (int i = 1; i < n; i++)
+	{
+		lmax[i] = max(lmax[i - 1], arr[i]);
+	}
+
+	for (int i = n - 2; i >= 0; i--)
+	{
+		rmax[i] = max(rmax[i + 1], arr[i]);
+	}
+
+	for (int i = 1; i < n - 1; i++)
+	{
+		res += min(lmax[i], rmax[i]) - arr[i];
+	}
+
+	cout << " ans through efficient solution is " << res;
+}
+int trapping_rain_twopointer(vector<int> arr)
 {
 }
 int main()
 {
 	vector<int> arr = {3, 0, 1, 2, 5};
-	trappingRain_naive(arr);
-	//cout << "Efficient :" << trappingRain_efficient(arr) << endl;
+	trappingRain_better(arr);
+	// cout << "Efficient :" << trappingRain_efficient(arr) << endl;
 
 	return 0;
 }
