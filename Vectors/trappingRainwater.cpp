748850b5e8e68d2cd6cf5a5bafa7d12bb11a3726
Aditya Ojha <aditya.ojha2820@gmail.com>
2024-03-23 18:38:58 -0400

    trapping rain water best method

--- Vectors/trappingRainwater.cpp
+++ Vectors/trappingRainwater.cpp
@@ -50,12 +50,36 @@ int trappingRain_better(vector<int> arr)
 }
 int trapping_rain_twopointer(vector<int> arr)
 {
+	int n = arr.size();
+	if (arr.empty())
+		return 0;
+
+	int lmax = arr[0];
+	int rmax = arr[n - 1];
+	int l = 0;
+	int r = n - 1;
+	int waterTrapped = 0;
+	while (l < r)
+	{
+		if (lmax > rmax)
+		{
+			l++;
+			lmax = max(arr[l], lmax);
+			waterTrapped += lmax - arr[l];
+		}
+		else
+		{
+			r--;
+			rmax = max(arr[r], rmax);
+			waterTrapped += rmax - arr[r];
+		}
+	}
+	cout << waterTrapped << endl;
+	return waterTrapped;
 }
 int main()
 {
-	vector<int> arr = {3, 0, 1, 2, 5};
-	trappingRain_better(arr);
-	// cout << "Efficient :" << trappingRain_efficient(arr) << endl;
-
+	vector<int> arr = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}; // Example input
+	cout << "Water trapped: " << trapping_rain_twopointer(arr) << endl;
 	return 0;
 }
